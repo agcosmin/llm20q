edefc211ed50e91ed678e9a3ab993a199299f9e6
Cosmin-George Alexandru <cosmingeorge.alexandru@gmail.com>
2024-07-23 23:24:26 +0300

    Improve tests readability

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -6,39 +6,58 @@ import pytest
 import llm20q.agents
 
 
-def test_construct_prompt_bilder_without_prompt_pattern_raises_ex(
-    chat_templates,
-):
-    user, model, model_start, *args = chat_templates
+def test_construct_prompt_bilder_without_prompt_pattern_raises_ex():
     with pytest.raises(ValueError):
-        llm20q.agents.PromptBuilder("user_prompt", model, model_start, *args)
+        llm20q.agents.PromptBuilder(
+            user_chat_template="user_bad_pattern",
+            model_chat_template="model:{prompt}\n",
+            model_chat_start="model:",
+        )
     with pytest.raises(ValueError):
-        llm20q.agents.PromptBuilder(user, "model_prompt", model_start, *args)
+        llm20q.agents.PromptBuilder(
+            user_chat_template="user:{prompt}",
+            model_chat_template="model_bad_pattern",
+            model_chat_start="model:",
+        )
 
 
-def test_build_description_with_no_questions_and_answers_results_in_empty_str(
-    prompt_builder,
-):
-    assert prompt_builder.build_description([], []) == ""
+def test_build_description_with_no_questions_and_answers_results_in_empty_str():
+    builder = llm20q.agents.PromptBuilder(
+        user_chat_template="user:{prompt}\n",
+        model_chat_template="model:{prompt}\n",
+        model_chat_start="model:",
+    )
+    assert builder.build_description([], []) == ""
 
 
-def test_build_desc_raises_for_mismatched_question_and_answers_length(
-    prompt_builder,
-):
+def test_build_desc_raises_for_mismatched_question_and_answers_length():
+    builder = llm20q.agents.PromptBuilder(
+        user_chat_template="user:{prompt}\n",
+        model_chat_template="model:{prompt}\n",
+        model_chat_start="model:",
+    )
     with pytest.raises(ValueError):
-        prompt_builder.build_description(["Is it a cow"], [])
+        builder.build_description(["Is it a cow"], [])
     with pytest.raises(ValueError):
-        prompt_builder.build_description([], ["Yes", "no"])
+        builder.build_description([], ["Yes", "no"])
 
 
-def test_build_description_with_invalid_question_raises_ex(prompt_builder):
+def test_build_description_with_invalid_question_raises_ex():
+    builder = llm20q.agents.PromptBuilder(
+        user_chat_template="user:{prompt}\n",
+        model_chat_template="model:{prompt}\n",
+        model_chat_start="model:",
+    )
     with pytest.raises(ValueError):
-        prompt_builder.build_description(["How many coins?"], ["yes"])
+        builder.build_description(["How many coins?"], ["yes"])
 
 
-def test_build_description_give_exepcted_results_for_all_question_types(
-    prompt_builder,
-):
+def test_build_description_give_exepcted_results_for_all_question_types():
+    builder = llm20q.agents.PromptBuilder(
+        user_chat_template="user:{prompt}\n",
+        model_chat_template="model:{prompt}\n",
+        model_chat_start="model:",
+    )
     samples = [
         ("Is it", "an animal?", "is an animal", "is not an animal"),
         ("Does it", "bark?", "does bark", "does not bark"),
@@ -47,123 +66,135 @@ def test_build_description_give_exepcted_results_for_all_question_types(
 
     for prefix, suffix, affirmative, negative in samples:
         question = prefix + " " + suffix
-        assert affirmative == prompt_builder.build_description(
-            [question], ["yes"]
-        )
-        assert negative == prompt_builder.build_description([question], ["no"])
+        assert builder.build_description([question], ["yes"]) == affirmative
+        assert builder.build_description([question], ["no"]) == negative
 
 
-def test_build_description_from_question_with_invalid_chars(prompt_builder):
+def test_build_description_from_question_with_invalid_chars():
+    builder = llm20q.agents.PromptBuilder(
+        user_chat_template="user:{prompt}\n",
+        model_chat_template="model:{prompt}\n",
+        model_chat_start="model:",
+    )
     question = ["Is it a*(big     *country?**?---?"]
-    assert "is a big country" == prompt_builder.build_description(
-        question, ["yes"]
-    )
-    assert "is not a big country" == prompt_builder.build_description(
-        question, ["no"]
-    )
+    assert builder.build_description(question, ["yes"]) == "is a big country"
+    assert builder.build_description(question, ["no"]) == "is not a big country"
 
 
-def test_build_chained_description(prompt_builder):
-    desc = prompt_builder.build_description(
+def test_build_chained_description():
+    builder = llm20q.agents.PromptBuilder(
+        user_chat_template="user:{prompt}\n",
+        model_chat_template="model:{prompt}\n",
+        model_chat_start="model:",
+    )
+    desc = builder.build_description(
         ["Is it an animal?", "Does it bark?", "Does it have a tail?"],
         ["yes", "no", "yes"],
     )
-    assert "is an animal, does not bark, does have a tail" == desc
+    assert desc == "is an animal, does not bark, does have a tail"
 
 
-def test_build_description_with_non_yes_or_no_answer(prompt_builder):
-    assert "" == prompt_builder.build_description(
-        ["Is it an animal?"], ["yes no"]
+def test_build_description_with_non_yes_or_no_answer():
+    builder = llm20q.agents.PromptBuilder(
+        user_chat_template="user:{prompt}\n",
+        model_chat_template="model:{prompt}\n",
+        model_chat_start="model:",
     )
-    assert "does bark" == prompt_builder.build_description(
+    assert builder.build_description(["Is it an animal?"], ["yes no"]) == ""
+    one_bad_one_good_answer_prompt = builder.build_description(
         ["Is it an animal?", "Does it bark?"], ["something someting***", "yes"]
     )
+    assert one_bad_one_good_answer_prompt == "does bark"
 
 
 @pytest.mark.parametrize(
     "answer", ["Yes", "yEs", "* yeS", "   YES  ()?", "ye_s"]
 )
-def test_build_description_with_non_standard_yes_answer(prompt_builder, answer):
-    assert "is an animal" == prompt_builder.build_description(
-        ["Is it an animal?"], [answer]
+def test_build_description_with_non_standard_yes_answer(answer):
+    builder = llm20q.agents.PromptBuilder(
+        user_chat_template="user:{prompt}\n",
+        model_chat_template="model:{prompt}\n",
+        model_chat_start="model:",
     )
+    assert builder.build_description(["Is it a dog?"], [answer]) == "is a dog"
 
 
 @pytest.mark.parametrize("answer", ["No", "nO", "* NO", "   NO  ()?", "n-o"])
-def test_build_description_with_non_standard_no_answer(prompt_builder, answer):
-    assert "is not an animal" == prompt_builder.build_description(
-        ["Is it an animal?"], [answer]
+def test_build_description_with_non_standard_no_answer(answer):
+    builder = llm20q.agents.PromptBuilder(
+        user_chat_template="user:{prompt}\n",
+        model_chat_template="model:{prompt}\n",
+        model_chat_start="model:",
+    )
+    assert (
+        builder.build_description(["Is it a dog?"], [answer]) == "is not a dog"
     )
 
 
-def test_interleave_dialog(prompt_builder):
+def test_interleave_dialog():
     # pylint: disable=protected-access
+    builder = llm20q.agents.PromptBuilder(
+        user_chat_template="user:{prompt}\n",
+        model_chat_template="model:{prompt}\n",
+        model_chat_start="model:",
+    )
     user_first_lines = ["User line", "Model line", "User line"]
-    user_first_dialog = prompt_builder._interleave_dialog(
+    user_first_dialog = builder._interleave_dialog(
         user_first_lines, model_first=False
     )
     user_first_expected_dialog = (
-        prompt_builder._user_chat_template.format(prompt=user_first_lines[0])
-        + prompt_builder._model_chat_template.format(prompt=user_first_lines[1])
-        + prompt_builder._user_chat_template.format(prompt=user_first_lines[2])
+        "user:User line\n" + "model:Model line\n" + "user:User line\n"
     )
-    assert user_first_expected_dialog == user_first_dialog
+    assert user_first_dialog == user_first_expected_dialog
 
     model_first_lines = ["Model line", "User line", "Model line"]
-    model_first_dialog = prompt_builder._interleave_dialog(
+    model_first_dialog = builder._interleave_dialog(
         model_first_lines, model_first=True
     )
     model_first_expected_dialog = (
-        prompt_builder._model_chat_template.format(prompt=model_first_lines[0])
-        + prompt_builder._user_chat_template.format(prompt=model_first_lines[1])
-        + prompt_builder._model_chat_template.format(
-            prompt=model_first_lines[2]
-        )
+        "model:Model line\n" + "user:User line\n" + "model:Model line\n"
     )
-    assert model_first_expected_dialog == model_first_dialog
+    assert model_first_dialog == model_first_expected_dialog
 
 
-def test_interleave_dialog_with_custom_separator(prompt_builder):
+def test_interleave_dialog_with_custom_separator():
     # pylint: disable=protected-access
+    builder = llm20q.agents.PromptBuilder(
+        user_chat_template="user:{prompt}\n",
+        model_chat_template="model:{prompt}\n",
+        model_chat_start="model:",
+    )
     lines = ["User line", "Model line", "User line"]
     sep = "@@@"
-    dialog = prompt_builder._interleave_dialog(
-        lines, sep=sep, model_first=False
-    )
+    dialog = builder._interleave_dialog(lines, sep=sep, model_first=False)
     expected_dialog = (
-        prompt_builder._user_chat_template.format(prompt=lines[0])
-        + sep
-        + prompt_builder._model_chat_template.format(prompt=lines[1])
-        + sep
-        + prompt_builder._user_chat_template.format(prompt=lines[2])
+        "user:User line\n@@@" + "model:Model line\n@@@" + "user:User line\n"
     )
-    assert expected_dialog == dialog
+    assert dialog == expected_dialog
 
 
 def test_generate_ask_prompt_with_fewshots():
     builder = llm20q.agents.PromptBuilder(
-        user_chat_template="<bos>user:{prompt}<eos>",
-        model_chat_template="<bos>model:{prompt}<eos>",
-        model_chat_start="<bos>model:",
-        ask_prompt_suffix="Ask a question about something that",
+        user_chat_template="user:{prompt}\n",
+        model_chat_template="model:{prompt}\n",
+        model_chat_start="model:",
+        ask_prompt_suffix="Ask a question:",
         ask_fewshots=[
-            "You must guess someting by asking questions about it."
-            + "Ask question about something that is a place, is in Europe",
-            "Is it near France?",
-            "Ask question about something that is not a place, does bark",
-            "Does it have a tail?",
+            "Describe ask action.User ask example 1",
+            "Model ask answer 1",
+            "User ask example 2",
+            "Model ask answer 2",
         ],
     )
     questions = ["Is it an animal?", "Does it meow?"]
     answers = ["yes", "no"]
     expected_prompt = (
-        "<bos>user:You must guess someting by asking questions about it."
-        + "Ask question about something that is a place, is in Europe<eos>"
-        + r"<bos>model:Is it near France\?<eos>"
-        + "<bos>user:Ask question about something that is not a place, does bark<eos>"  # pylint: disable=line-too-long
-        + r"<bos>model:Does it have a tail\?<eos>"
-        + "<bos>user:Ask a question about something that is an animal, does not meow<eos>"  # pylint: disable=line-too-long
-        + "<bos>model:(Is it|Does it have|Does it)"
+        "user:Describe ask action.User ask example 1\n"
+        + "model:Model ask answer 1\n"
+        + "user:User ask example 2\n"
+        + "model:Model ask answer 2\n"
+        + "user:Ask a question: is an animal, does not meow\n"
+        + "model:(Is it|Does it have|Does it)"
     )
     prompt = builder.ask(questions, answers)
     assert re.fullmatch(expected_prompt, prompt)
@@ -171,16 +202,16 @@ def test_generate_ask_prompt_with_fewshots():
 
 def test_generate_ask_prompt_without_fewshots():
     builder = llm20q.agents.PromptBuilder(
-        user_chat_template="<bos>user:{prompt}<eos>",
-        model_chat_template="<bos>model:{prompt}<eos>",
-        model_chat_start="<bos>model:",
-        ask_prompt_suffix="Ask a question about something that",
+        user_chat_template="user:{prompt}\n",
+        model_chat_template="model:{prompt}\n",
+        model_chat_start="model:",
+        ask_prompt_suffix="Ask a question:",
     )
     questions = ["Is it an animal?", "Does it meow?"]
     answers = ["yes", "no"]
     expected_prompt = (
-        "<bos>user:Ask a question about something that is an animal, does not meow<eos>"  # pylint: disable=line-too-long
-        + "<bos>model:(Is it|Does it have|Does it)"
+        "user:Ask a question: is an animal, does not meow\n"
+        + "model:(Is it|Does it have|Does it)"
     )
     prompt = builder.ask(questions, answers)
     assert re.fullmatch(expected_prompt, prompt)
@@ -188,29 +219,27 @@ def test_generate_ask_prompt_without_fewshots():
 
 def test_generate_guess_prompt_with_fewshots():
     builder = llm20q.agents.PromptBuilder(
-        user_chat_template="<bos>user:{prompt}<eos>",
-        model_chat_template="<bos>model:{prompt}<eos>",
-        model_chat_start="<bos>model:",
+        user_chat_template="user:{prompt}\n",
+        model_chat_template="model:{prompt}\n",
+        model_chat_start="model:",
         guess_prompt_prefix="Name something that",
         guess_prompt_suffix="The answer is",
         guess_fewshots=[
-            "You must name someting based on the following description."
-            + "Name something that is a place, is in Europe",
-            "The answer is France",
-            "Name something that is not a place, does bark",
-            "The answer is a dog",
+            "Describe guess action. User guess example 1",
+            "Model guess example 1",
+            "User guess example 2",
+            "Model guess example 2",
         ],
     )
     questions = ["Is it an animal?", "Does it meow?"]
     answers = ["yes", "no"]
     expected_prompt = (
-        "<bos>user:You must name someting based on the following description."
-        + "Name something that is a place, is in Europe<eos>"
-        + r"<bos>model:The answer is France<eos>"
-        + "<bos>user:Name something that is not a place, does bark<eos>"
-        + r"<bos>model:The answer is a dog<eos>"
-        + "<bos>user:Name something that is an animal, does not meow<eos>"
-        + "<bos>model:The answer is"
+        "user:Describe guess action. User guess example 1\n"
+        + "model:Model guess example 1\n"
+        + "user:User guess example 2\n"
+        + "model:Model guess example 2\n"
+        + "user:Name something that is an animal, does not meow\n"
+        + "model:The answer is"
     )
     prompt = builder.guess(questions, answers)
     assert prompt == expected_prompt
@@ -218,17 +247,17 @@ def test_generate_guess_prompt_with_fewshots():
 
 def test_generate_guess_prompt_without_fewshots():
     builder = llm20q.agents.PromptBuilder(
-        user_chat_template="<bos>user:{prompt}<eos>",
-        model_chat_template="<bos>model:{prompt}<eos>",
-        model_chat_start="<bos>model:",
+        user_chat_template="user:{prompt}\n",
+        model_chat_template="model:{prompt}\n",
+        model_chat_start="model:",
         guess_prompt_prefix="Name something that",
         guess_prompt_suffix="The answer is",
     )
     questions = ["Is it an animal?", "Does it meow?"]
     answers = ["yes", "no"]
     expected_prompt = (
-        "<bos>user:Name something that is an animal, does not meow<eos>"
-        + "<bos>model:The answer is"
+        "user:Name something that is an animal, does not meow\n"
+        + "model:The answer is"
     )
     prompt = builder.guess(questions, answers)
     assert prompt == expected_prompt
@@ -247,31 +276,26 @@ def test_generate_guess_prompt_without_fewshots():
 )
 def test_generate_answer_prompt(question, it_word):
     builder = llm20q.agents.PromptBuilder(
-        user_chat_template="<bos>user:{prompt}<eos>",
-        model_chat_template="<bos>model:{prompt}<eos>",
-        model_chat_start="<bos>model:",
+        user_chat_template="user:{prompt}\n",
+        model_chat_template="model:{prompt}\n",
+        model_chat_start="model:",
     )
     keyword = "dog"
     prompt = builder.answer(keyword, question.format(it_word=it_word))
     expected_prompt = (
-        "<bos>user:Yes or no."
-        + question.format(it_word=keyword)
-        + "<eos>"
-        + "<bos>model:"
+        f"user:Yes or no.{question.format(it_word=keyword)}\nmodel:"
     )
     assert expected_prompt == prompt
 
 
 def test_generate_answer_prompt_with_dirty_question():
     builder = llm20q.agents.PromptBuilder(
-        user_chat_template="<bos>user:{prompt}<eos>",
-        model_chat_template="<bos>model:{prompt}<eos>",
-        model_chat_start="<bos>model:",
+        user_chat_template="user:{prompt}\n",
+        model_chat_template="model:{prompt}\n",
+        model_chat_start="model:",
     )
     keyword = "dog"
     question = "Does+the,+ it+, have a hat???"
     prompt = builder.answer(keyword, question)
-    expected_prompt = (
-        "<bos>user:Yes or no.Does the dog have a hat?<eos>" + "<bos>model:"
-    )
+    expected_prompt = "user:Yes or no.Does the dog have a hat?\nmodel:"
     assert expected_prompt == prompt
